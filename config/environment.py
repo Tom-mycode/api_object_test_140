7cd39077abd3930f6019cf4f50c352558c4fc758
MuMingYang <16672074545@163.com>
2026-04-23 17:18:46 +0800

    email_send

--- config/environment.py
+++ config/environment.py
@@ -2,7 +2,7 @@ ip_address = "http://192.168.150.134:8080"
 db_host = '192.168.150.134'
 
 # 邮箱
-sender_email = "16672074545@163.com"
-password_email = 'MYfL5vBwTZfjbS7A'
-EMAIL_SERVER = "smtp.163.com"  # 163邮箱
+sender_email = "3382109179@qq.com"
+password_email = 'harpnhhrqctzcjif'
+EMAIL_SERVER = "smtp.qq.com"  # 163邮箱
 PORT = 587
